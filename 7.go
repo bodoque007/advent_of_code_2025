1bcd630ccc934f0337162f1517eb98c855623061
Sebastian Sarco <sebastiansarco20@gmail.com>
2025-12-12 16:52:20 -0300

    Solved using simulation
    Refactor visited tracking and improve beam handling logic.

--- 7.go
+++ 7.go
@@ -9,7 +9,7 @@ import (
 
 type Coordinate struct {
 	Row int
-	Col int 
+	Col int
 }
 
 type TupleSet map[Coordinate]struct{}
@@ -22,7 +22,7 @@ func main() {
 
 	scanner.Scan()
 	first := scanner.Text()
-	
+
 	startingBeamCol := strings.Index(first, "S")
 	beams := []Coordinate{
 		{0, startingBeamCol},
@@ -37,29 +37,32 @@ func main() {
 
 	heightBoard := len(board)
 	splitCount := 0
-	visitedSplits := make(TupleSet)
+	visited := make(TupleSet)
 
 	for len(beams) > 0 {
 		last := len(beams) - 1
 		b := beams[last]
 		beams = beams[:last]
+		if _, ok := visited[b]; ok {
+			continue
+		}
+		visited[b] = struct{}{}
+
 		nextRow := b.Row + 1
 		if nextRow < heightBoard {
 			if board[nextRow][b.Col] == '^' {
-				beams = append(beams, Coordinate{
-					nextRow,
-					b.Col + 1,
-				}, Coordinate{
-					nextRow,
-					b.Col - 1,
-				})
+				if b.Col+1 >= 0 && b.Col+1 < len(board[nextRow]) {
+					beams = append(beams, Coordinate{nextRow, b.Col + 1})
+				}
+				if b.Col-1 >= 0 && b.Col-1 < len(board[nextRow]) {
+					beams = append(beams, Coordinate{nextRow, b.Col - 1})
+				}
 				split := Coordinate{
 					nextRow,
 					b.Col,
 				}
-				if _, exists := visitedSplits[split]; !exists {
+				if _, exists := visited[split]; !exists {
 					splitCount++
-					visitedSplits[split] = struct{}{}
 				}
 			} else {
 				beams = append(beams, Coordinate{
